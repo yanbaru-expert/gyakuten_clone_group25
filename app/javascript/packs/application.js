d2409f8f05f7e96fae2fb740153acd9ed44d1ea0
sandaka619 <freestyleworking8@gmail.com>
2020-07-12 00:10:07 +0900

    8_Bootstrapの導入_2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,5 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// Bootstrapで必要なファイルを以下追加
+require("bootstrap/dist/js/bootstrap")
