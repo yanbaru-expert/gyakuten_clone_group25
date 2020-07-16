c595fabfc75c7f6a56b0d6ad30c1842d6f2205e6
sandaka619 <freestyleworking8@gmail.com>
2020-07-16 13:48:40 +0900

    Bootstrap導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-// ***** 以下を追加 *****
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery'
-}))
-// ***** 以上を追加 *****
-
 module.exports = environment
