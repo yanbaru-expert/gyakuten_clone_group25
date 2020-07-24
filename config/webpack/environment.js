fdfc94ae31b88870d820710dc1e8ef74cbcca02c
sandaka619 <freestyleworking8@gmail.com>
2020-07-24 14:12:59 +0900

    add flash-4

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery'
-}))
-
 module.exports = environment
