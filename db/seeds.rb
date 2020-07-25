1ba88535783a653ad8475f29481f2becec62c311
浅野一輝 <asanokazuki@asanoikkinoMacBook-Pro.local>
2020-07-25 14:13:37 +0900

    管理者のメールアドレスとパスワードを定数に

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ User.find_or_create_by!(email: EMAIL) do |user|
   puts 'ユーザーの初期データインポートに成功しました。'
 end
 # 管理者画面
-AdminUser.find_or_create_by!(email: 'test@example.com') do |user|
-  user.password = "password"
-  password_confirmation = 'password'
+AdminUser.find_or_create_by!(email: EMAIL) do |user|
+  user.password = PASSWORD
+  password_confirmation = PASSWORD
 end
