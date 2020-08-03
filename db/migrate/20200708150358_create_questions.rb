e7133d5c14c3076bc969fce2695ff6d533efe3e7
浅野一輝 <asanokazuki@asanoikkinoMacBook-Pro.local>
2020-08-03 12:14:22 +0900

    nullカラムの追加/migration/link_toへの書き換え

--- db/migrate/20200708150358_create_questions.rb
+++ db/migrate/20200708150358_create_questions.rb
@@ -1,10 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-      t.string :title, null: false
-      t.text :detail, null: false
+      t.string :title
+      t.text :detail
 
       t.timestamps
     end
+    change_column_null(:questions, :title, false)
+    change_column_null(:questions, :detail, false)
   end
 end
