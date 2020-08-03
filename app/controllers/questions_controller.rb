9dac40df3dd91dd7ac26ead5306c8c1572fbd22d
sandaka619 <freestyleworking8@gmail.com>
2020-08-03 13:14:01 +0900

    add Question-1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,27 @@
 class QuestionsController < ApplicationController
   def index
-    @questions = Question.order(created_at: "DESC")
+    @questions = Question.order(id: :desc)
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path, notice: "質問を受け付けました。"
+    else
+      @questions = Question.order(id: :desc)
+      flash.now[:alert] = "記入漏れがあります。"
+      render :index
+    end
+  end
+
+  def show
+      @question = Question.find(params[:id])
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :detail)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "movies#index"
   resources :aws_texts
-  resources :questions
+  resources :questions, only: [:index, :show, :create] do
+    resources :solutions, only: [:index, :create]
+  end
 end
