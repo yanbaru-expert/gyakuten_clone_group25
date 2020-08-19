5f07084f7eb27f00ef18a2af011905adc9382bb8
yuta <fantasista098@gmail.com>
2020-08-19 18:29:14 +0900

    rails db:rollbackにて一つ前の不要なテーブルを作成するマイグレーションファイルを取り消し削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_230946) do
+ActiveRecord::Schema.define(version: 2020_08_10_044924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,14 +39,6 @@ ActiveRecord::Schema.define(version: 2020_08_18_230946) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-  end
-
-  create_table "all_movies", force: :cascade do |t|
-    t.string "category", null: false
-    t.string "title", null: false
-    t.string "url", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "aws_texts", force: :cascade do |t|
