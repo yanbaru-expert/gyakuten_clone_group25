08758f7cb726710c9f8581f9ec22aa00555880aa
yuta <fantasista098@gmail.com>
2020-08-21 16:51:50 +0900

    再々リファクタリング

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,13 +3,13 @@ class Movie < ApplicationRecord
   validates :url, presence: true
   validates :category, presence: true
 
-  SELECTION_MOVIIES = %W[Basic Git Ruby #{"Ruby on Rails"}]
+  SELECTION_MOVIIES = ["Basic", "Git", "Ruby", "Ruby on Rails"]
 
   def self.select_movies(category)
     if category.present?
       where(category: category)
     else
-      where("category IN(?)", SELECTION_MOVIIES)
+      where(category: SELECTION_MOVIIES)
     end
   end
 end
