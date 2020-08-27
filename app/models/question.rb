85180b840f9a4398b0edd128577423bd737baf36
ajk0421 <43706278+ajk0421@users.noreply.github.com>
2020-08-27 16:29:32 +0900

    Revert "質問一覧、詳細ページへのView数の表示を実装"

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,4 @@
 class Question < ApplicationRecord
-  is_impressionable counter_cache: true
   has_many :solutions, dependent: :destroy
   validates :title, presence: true
   validates :detail, presence: true
