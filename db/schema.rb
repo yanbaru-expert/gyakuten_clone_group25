83b5cd7d635838c3c92f318b3209a2b5d84d9c8f
yuta <fantasista098@gmail.com>
2020-08-30 08:24:43 +0900

    回答に関するいいね機能の実装の下準備（中間テーブル、コントローラーの作成及びルーティングの設定）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_003509) do
+ActiveRecord::Schema.define(version: 2020_08_29_231435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(version: 2020_08_26_003509) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "solution_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["solution_id"], name: "index_favorites_on_solution_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "impressions", force: :cascade do |t|
@@ -134,5 +143,7 @@ ActiveRecord::Schema.define(version: 2020_08_26_003509) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "solutions"
+  add_foreign_key "favorites", "users"
   add_foreign_key "solutions", "questions"
 end
