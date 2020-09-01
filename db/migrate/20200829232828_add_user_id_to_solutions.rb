520c2ca32af987e349b71ba806c19106b6aa8ffb
yuta <fantasista098@gmail.com>
2020-09-02 08:13:37 +0900

    userとsolutionとの関連付けを行うためsolutionにuser_idカラムを追加したものを不要なため削除

--- db/migrate/20200829232828_add_user_id_to_solutions.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdToSolutions < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :solutions, :user, null: false, foreign_key: true, default: 1
-    # change_column_null :solutions, :user_id, false
-    #Ex:- change_column("admin_users", "email", :string, :limit =>25)
-  end
-end
