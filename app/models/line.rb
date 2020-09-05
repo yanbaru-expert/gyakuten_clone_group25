a8dcfcc97ddf72df29ed817a6d21df2770daa1dd
浅野一輝 <asanokazuki@asanoikkinoMacBook-Pro.local>
2020-09-05 18:06:10 +0900

    user,lineモデルに追記

--- app/models/line.rb
+++ app/models/line.rb
@@ -2,4 +2,9 @@ class Line < ApplicationRecord
   validates :genre, presence: true
   validates :title, presence: true
   validates :contents, presence: true
+  has_many :reads
+  
+  def read_by?(user_id)
+    reads.where(user_id: user_id).exists?
+  end
 end
