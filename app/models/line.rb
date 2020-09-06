7e6588b9364ff398065f23ad4f3f6f55451c9aaf
ajk0421 <ajk.spirituallounge@gmai.com>
2020-09-06 13:40:36 +0900

    既読実装サンプル

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,4 +1,6 @@
 class Line < ApplicationRecord
+  has_many :reads, as: :readable
+
   validates :genre, presence: true
   validates :title, presence: true
   validates :contents, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_solutions, through: :favorites, source: :soulution
+  has_many :reads
+  has_many :line_reads, through: :reads, :source => :readable,:source_type => 'Line'
+
+  # MEMO: textに実装するなら以下の記述
+  # has_many :text_reads, through: :reads, :source => :readable,:source_type => 'Text'
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
