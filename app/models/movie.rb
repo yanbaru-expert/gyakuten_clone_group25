f3444173859881d25d09188bd6410624ecf25a62
浅野一輝 <asanokazuki@asanoikkinoMacBook-Pro.local>
2020-09-09 22:12:55 +0900

    user,movieモデルの記述変更

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   validates :url, presence: true
   validates :category, presence: true
+  has_many :checks, as: :checkable
 
   SELECTION_MOVIIES = ["Basic", "Git", "Ruby", "Ruby on Rails"]
 
@@ -11,5 +12,9 @@ class Movie < ApplicationRecord
     else
       where(category: SELECTION_MOVIIES)
     end
+  end
+
+  def checked_by?(user)
+    checks.where(user_id:user.id).exists?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_solutions, through: :favorites, source: :soulution
+  has_many :checks
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
