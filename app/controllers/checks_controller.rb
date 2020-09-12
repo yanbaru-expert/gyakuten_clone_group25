afae98354b4d534cacb4a410fde02d37962e055e
浅野一輝 <asanokazuki@asanoikkinoMacBook-Pro.local>
2020-09-12 22:28:51 +0900

    ajax対応にする為、パーシャル、jsファイルなどを作成

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,14 +1,12 @@
 class ChecksController < ApplicationController
   def create
-    @movie=Movie.find_by(id: params[:movie_id])
+    @movie=Movie.find(params[:movie_id])
     Check.create(user_id:current_user.id,checkable:@movie)
-    redirect_back(fallback_location: movies_path)
   end
   
   def destroy
-    @movie=Movie.find_by(id: params[:movie_id])
+    @movie=Movie.find(params[:movie_id])
     check=Check.find_by(user_id:current_user.id,checkable:@movie)
     check.destroy
-    redirect_back(fallback_location: movies_path)
   end
 end
