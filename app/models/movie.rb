e0d0e0b0ee8f7225be2bf7bd416ba0511d2fa4e7
浅野一輝 <asanokazuki@asanoikkinoMacBook-Pro.local>
2020-09-14 20:15:27 +0900

    movieモデル,ビューの記述の変更

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   validates :url, presence: true
   validates :category, presence: true
+  has_many :reads, as: :readable
 
   SELECTION_MOVIIES = ["Basic", "Git", "Ruby", "Ruby on Rails"]
 
@@ -11,5 +12,9 @@ class Movie < ApplicationRecord
     else
       where(category: SELECTION_MOVIIES)
     end
+  end
+
+  def read_by?(user_id)
+    reads.where(user_id:user_id).exists?
   end
 end
