b069ad875a66cb24ed9760dcdc36514546f4ef61
浅野一輝 <asanokazuki@asanoikkinoMacBook-Pro.local>
2020-09-14 20:14:12 +0900

    readコントローラーの作成

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -1,2 +1,12 @@
 class ReadsController < ApplicationController
+  def create
+    @movie = Movie.find(params[:movie_id])
+    Read.create(user_id:current_user.id,readable:@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:movie_id])
+    read = Read.find_by(user_id:current_user.id,readable:@movie)
+    read.destroy
+  end
 end
