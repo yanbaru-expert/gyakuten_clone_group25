ff19d0426168ec52e0b0e1eea18cb6350f21c3d3
t-yuzo <taguchiyuuzou@My-MBP.local>
2020-09-16 03:26:09 +0900

    検索機能の実装

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,11 +1,13 @@
 class TextsController < ApplicationController
 
   def index
-    @texts = Text.all
+  @texts = Text.all
+  @q = Text.ransack(params[:q])
+  @texts = @q.result(distinct: true)
   end
 
   def show
     @texts = Text.find(params[:id])
   end
 
-end 
+end
