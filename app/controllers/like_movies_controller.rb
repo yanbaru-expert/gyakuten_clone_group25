e7e45cd06130db07338834427dd8876e2a60569c
t-yuzo <taguchiyuuzou@My-MBP.local>
2020-10-13 04:03:00 +0900

    Like_movieコントローラー実装

--- app/controllers/like_movies_controller.rb
+++ app/controllers/like_movies_controller.rb
@@ -1,7 +1,13 @@
 class LikeMoviesController < ApplicationController
   def create
+    like_movie = current_user.like_movies.build(movie_id: params[:movie_id])
+    like_movie.save
+    redirect_to movies_path
   end
 
   def destroy
+    like_movie = Like_movie.find_by(movie_id: params[:movie_id], user_id: current_user.id)
+    like_movie.destroy
+    redirect_to movies_path
   end
 end
